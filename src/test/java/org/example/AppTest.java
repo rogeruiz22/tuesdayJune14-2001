38897d7e3c47fd2359a582e38a71fdfa3d5d4722
Roger Ruiz <roger.ruiz@oktana.com>
2022-06-16 23:23:23 -0400

    yep

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -1,20 +1,63 @@
 package org.example;
 
-import static org.junit.Assert.assertTrue;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.Test;
 
-import org.junit.Test;
-
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
+public class AppTest {
+    WebDriver driver = new ChromeDriver();
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void testingCaptiveSiteSuccesfulConnection() throws Exception {
+        driver.get("http://captive.apple.com/");
+
+        WebElement labelMessage = driver.findElement(By.xpath("//body[contains(text(), 'Success')]"));
+
+        String message = labelMessage.getText();
+
+        Thread.sleep(1000);
+
+        Assert.assertEquals(message, "Success", "Internet Issues");
+    }
+
+    @Test
+    public void verifyIntranetSuccessfulLogin() throws Exception {
+        driver.get("https://oktana.force.com/s/");
+
+        Thread.sleep(1000);
+
+        WebElement oktanaGmailLoginButton = driver.findElement(By.xpath("//button[@title='Oktana_Gmail']"));
+        oktanaGmailLoginButton.click();
+
+        Thread.sleep(2000);
+
+        WebElement emailField = driver.findElement(By.xpath("//input[@type='email']"));
+        emailField.sendKeys("roger.ruiz@oktana.com");
+
+        WebElement nextButton = driver.findElement(By.xpath("//span[contains(text(), 'Next')]"));
+        nextButton.click();
+
+        Thread.sleep(2000);
+
+        WebElement passwordField = driver.findElement(By.xpath("//input[@type='password']"));
+        passwordField.sendKeys("Oktana@987632149");
+
+        WebElement passwordNextButton = driver.findElement(By.xpath("//span[contains(text(), 'Next')]/ancestor::button"));
+        passwordNextButton.click();
+
+        Thread.sleep(25000);
+
+        String pageTitle = driver.getTitle();
+
+        Assert.assertEquals(pageTitle, "Home", "You didn't accept the 2 verification step with your phone");
+
+    }
+
+    @AfterTest
+    public void tearDown() {
+        driver.quit();
     }
 }
